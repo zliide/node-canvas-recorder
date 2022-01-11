8b3631f9b7b53f85dd570bec683e9f39e7d4a988
Kristoffer Vinther <kristoffer@zliide.com>
2022-01-11 18:58:37 +0100

    Only one context type allowed per element.

--- index.ts
+++ index.ts
@@ -8,15 +8,23 @@ export function installCanvasRecorder(window: any, textMeasure: TextMeasure) {
     window.document.createElement = (localName: string) => {
         const inner = innerElementFactory(localName)
         if (localName.toLowerCase() === 'canvas') {
-            const context2D = new Recording2DCanvas(`_c${++canvasIndex}`, inner, textMeasure)
-            const contextWebGL = new RecordingWebGLCanvas(`_cgl${++canvasIndex}`, inner)
-            inner.id = `_cnvs${canvasIndex}`
+            let context: any
+            let contextType: string | undefined
+            const ix = canvasIndex++
+            inner.id = `_cnvs${ix}`
             inner.getContext = (type: string) => {
+                if (context) {
+                    if (type !== contextType) {
+                        return null
+                    }
+                    return context
+                }
+                contextType = type
                 switch (type) {
                     case '2d':
-                        return context2D
+                        return context = new Recording2DCanvas(`_c${ix}`, inner, textMeasure)
                     case 'webgl':
-                        return contextWebGL
+                        return context = new RecordingWebGLCanvas(`_cgl${ix}`, inner)
                     default:
                         return null
                 }
@@ -40,14 +48,14 @@ export function scriptPlayingRecordedCanvases(window: any) {
     for (let ix = 0; ix !== canvasTags.length; ++ix) {
         const canvas = canvasTags.item(ix)!
         const context2D = canvas.getContext('2d')
-        const script2D = context2D.script(canvas.id)
+        const script2D = context2D?.script(canvas.id)
         if (script2D) {
-            script += `const ${context2D.varName}=document.getElementById(${stringArg(canvas.id)}).getContext('2d');\r\n${script2D}`
+            script += `const ${context2D.varName}=document.getElementById(${stringArg(canvas.id)}).getContext('2d');\r\n${script2D}\r\n`
         }
         const contextWebGL = canvas.getContext('webgl')
-        const scriptGL = contextWebGL.script(canvas.id)
+        const scriptGL = contextWebGL?.script(canvas.id)
         if (scriptGL) {
-            script += `const ${contextWebGL.varName}=document.getElementById(${stringArg(canvas.id)}).getContext('webgl');\r\n${scriptGL}`
+            script += `const ${contextWebGL.varName}=document.getElementById(${stringArg(canvas.id)}).getContext('webgl');\r\n${scriptGL}\r\n`
         }
     }
     return script
--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,20 @@ import assert from 'assert'
 import { installCanvasRecorder, scriptPlayingRecordedCanvases } from '../index.js'
 
 describe('recording canvas', () => {
-    it('can record some 2d', async () => {
+    it('selects context type', () => {
+        const window = { document: new DummyDocument() } as any as Window
+        installCanvasRecorder(window, measureText)
+
+        const element2d = window.document.createElement('canvas')
+        assert.ok(element2d.getContext('2d'))
+        assert.strictEqual(element2d.getContext('webgl'), null)
+
+        const elementWebgl = window.document.createElement('canvas')
+        assert.ok(elementWebgl.getContext('webgl'))
+        assert.strictEqual(elementWebgl.getContext('2d'), null)
+    })
+
+    it('can record some 2d', () => {
         const window = { document: new DummyDocument() } as any as Window
         installCanvasRecorder(window, measureText)
 
@@ -14,13 +27,13 @@ describe('recording canvas', () => {
 
         const script = scriptPlayingRecordedCanvases(window)
         assert.deepStrictEqual(script.split('\r\n'), [
-            `const _c1=document.getElementById('_cnvs2').getContext('2d');`,
-            `_c1.quadraticCurveTo(0,1,2,3);`,
-            `_c1.fillText('Don\\\'t forget to escape!',4,5);`,
+            `const _c0=document.getElementById('_cnvs0').getContext('2d');`,
+            `_c0.quadraticCurveTo(0,1,2,3);`,
+            `_c0.fillText('Don\\\'t forget to escape!',4,5);`,
         ])
     })
 
-    it('can record some webgl', async () => {
+    it('can record some webgl', () => {
         const window = { document: new DummyDocument() } as any as Window
         installCanvasRecorder(window, measureText)
 
@@ -35,12 +48,12 @@ describe('recording canvas', () => {
 
         const script = scriptPlayingRecordedCanvases(window)
         assert.deepStrictEqual(script.split('\r\n'), [
-            "const _cgl2=document.getElementById('_cnvs2').getContext('webgl');",
-            'const _cgl2_0 = _cgl2.createBuffer();',
-            '_cgl2.bindBuffer(_cgl2.ARRAY_BUFFER, _cgl2_0);',
-            '_cgl2.flush();',
-            '_cgl2.bindBuffer(_cgl2.ARRAY_BUFFER, null);',
-            "_cgl2.getExtension('WEBGL_lose_context').loseContext();",
+            "const _cgl0=document.getElementById('_cnvs0').getContext('webgl');",
+            'const _cgl0_0 = _cgl0.createBuffer();',
+            '_cgl0.bindBuffer(_cgl0.ARRAY_BUFFER, _cgl0_0);',
+            '_cgl0.flush();',
+            '_cgl0.bindBuffer(_cgl0.ARRAY_BUFFER, null);',
+            "_cgl0.getExtension('WEBGL_lose_context').loseContext();",
         ])
     })
 })
